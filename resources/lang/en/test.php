d5f4c57dca814b7ec2fb3e4fa783700bcbe885f7
ledinhhuan <ledinhhuan205@gmail.com>
2019-03-22 15:50:45 +0700

    Store test question and show result

---
+++ resources/lang/en/test.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'number_of_correct' => 'Number of correct',
+    'result_details' => 'Result Details',
+    'finish_time' => 'Finish time',
+    'test_again' => 'Test Again!',
+];
